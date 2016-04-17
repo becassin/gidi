2da0464d851846ee6963b84a00a55f30dc91c27a
Guillaume <bonjour@giom.fr>
2016-04-17 19:15:22 +0200

    edits

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -656,6 +656,7 @@ if ($settings['hash_salt']) {
 /**
  * Load services definition file.
  */
+error_log(__DIR__);
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
@@ -714,9 +715,9 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  *
  * Keep this code block at the end of this file to take full effect.
  */
-# if (file_exists(__DIR__ . '/settings.local.php')) {
-#   include __DIR__ . '/settings.local.php';
-# }
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
 $databases['default']['default'] = array (
   'database' => 'gidi',
   'username' => 'root',
